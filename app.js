1f83ddfa57ef975cabf79dbf832311606ce32446
Robbyqt <65458308+Robbyqt@users.noreply.github.com>
2021-07-01 13:02:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ function remUnicode (str) {
     {'base':'x','letters':/[\uFF92\u0078\u24E7\uFF58\u1E8B\u1E8D]/g},
     {'base':'y','letters':/[\u0443\u0079\u24E8\uFF59\u1EF3\u00FD\u0177\u1EF9\u0233\u1E8F\u00FF\u1EF7\u1E99\u1EF5\u01B4\u024F\u1EFF]/g},
     {'base':'z','letters':/[\u007A\u24E9\uFF5A\u017A\u1E91\u017C\u017E\u1E93\u1E95\u01B6\u0225\u0240\u2C6C\uA763]/g},
-    {'base':'','letters':/[\u30C4\u30F0\u4E97\u0724\u10E6\u04DD]/g}
+    {'base':'','letters':/[\uFC5E\u30C4\u30F0\u4E97\u0724\u10E6\u04DD]/g}
   ];
 
   for(var i=0; i<defaultUnicodeRemovalMap.length; i++) {
